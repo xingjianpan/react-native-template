285fa926f0c6fc40627eaea2df94e6fea0dceceb
deric <github@mail.xingjian.me>
2017-03-20 12:43:04 +0800

    code cleanse

--- Router.js
+++ Router.js
@@ -14,56 +14,79 @@ const RouterComponent = () => {
         <Scene key="root" hideNavBar >
           <Scene key="tabbar" component={NavigationDrawer} >
             <Scene
-              key="home" tabs={true} default="Scene2"
-              tabBarStyle={{ backgroundColor: APP_STYLES.primaryColor, height: 40}}
-              tabBarSelectedItemStyle={{backgroundColor: APP_STYLES.primaryColor, height: 40}}
+              key="home" tabs default="main"
+              tabBarStyle={{ backgroundColor: APP_STYLES.primaryColor, height: 40 }}
+              tabBarSelectedItemStyle={{ backgroundColor: APP_STYLES.primaryColor, height: 40 }}
               navigationBarStyle={{ backgroundColor: APP_STYLES.primaryColor }}
             >
-              <Scene key="main"
-                      icon={TabIcon}
-                      selectedTabIcon="ios-home"
-                      tabIcon="ios-home-outline"
-                      navigationBarStyle={{ backgroundColor:APP_STYLES.primaryColor }}
-                      titleStyle={{ color:'white' }}
+              <Scene
+                key="main"
+                icon={TabIcon}
+                selectedTabIcon="ios-home"
+                tabIcon="ios-home-outline"
+                navigationBarStyle={{ backgroundColor: APP_STYLES.primaryColor }}
+                titleStyle={{ color: 'white' }}
               >
-                <Scene key="categories" component={MyScene} initial={true} hideNavBar={false} type="reset"/>
+                <Scene key="categories" component={MyScene} initial hideNavBar={false} type="reset" />
                 <Scene key="categoryEntity" component={MyScene} />
                 <Scene key="companyEntity" component={MyScene} />
                 <Scene key="appointmentContainer" component={MyScene} />
               </Scene>
 
-
-              <Scene key="maps" component={MyScene} icon={TabIcon}
-                     title="Map"
-                     selectedTabIcon="ios-map" tabIcon="ios-map-outline"
-                     navigationBarStyle={{ backgroundColor:APP_STYLES.primaryColor }}
-                     titleStyle={{ color:'white' }}
+              <Scene
+                key="maps"
+                component={MyScene}
+                icon={TabIcon}
+                title="Map"
+                selectedTabIcon="ios-map"
+                tabIcon="ios-map-outline"
+                navigationBarStyle={{ backgroundColor: APP_STYLES.primaryColor }}
+                titleStyle={{ color: 'white' }}
               />
 
-              <Scene key="favorites" component={MyScene} icon={TabIcon} title="Favorites"
-                     selectedTabIcon="ios-star" tabIcon="ios-star-outline"
-                     navigationBarStyle={{ backgroundColor:APP_STYLES.primaryColor }}
-                     titleStyle={{ color:'white' }}
+              <Scene
+                key="favorites"
+                component={MyScene}
+                icon={TabIcon}
+                title="Favorites"
+                selectedTabIcon="ios-star"
+                tabIcon="ios-star-outline"
+                navigationBarStyle={{ backgroundColor: APP_STYLES.primaryColor }}
+                titleStyle={{ color: 'white' }}
               />
 
-              <Scene key="appointments" component={MyAnotherScene}  icon={TabIcon} title="My Appointments"
-                     selectedTabIcon="ios-alarm" tabIcon="ios-alarm-outline"
-                     navigationBarStyle={{ backgroundColor:APP_STYLES.primaryColor }}
-                     titleStyle={{ color:'white' }}
-
+              <Scene
+                key="appointments"
+                component={MyAnotherScene}
+                icon={TabIcon}
+                title="My Appointments"
+                selectedTabIcon="ios-alarm"
+                tabIcon="ios-alarm-outline"
+                navigationBarStyle={{ backgroundColor: APP_STYLES.primaryColor }}
+                titleStyle={{ color: 'white' }}
               />
-              <Scene  key="settings" icon={TabIcon}  title="Settings"
-                    selectedTabIcon="ios-settings" tabIcon="ios-settings-outline"
-                    navigationBarStyle={{ backgroundColor:APP_STYLES.primaryColor }}
-                    titleStyle={{ color:'white' }}
+
+              <Scene
+                key="settings"
+                icon={TabIcon}
+                title="Settings"
+                selectedTabIcon="ios-settings"
+                tabIcon="ios-settings-outline"
+                navigationBarStyle={{ backgroundColor: APP_STYLES.primaryColor }}
+                titleStyle={{ color: 'white' }}
               >
                 <Scene key="settingsScene" component={MyAnotherScene} title="Settings"/>
-                <Scene key="term" component={MyAnotherScene}  hideTabBar={true} hideNavBar={true} />
-                <Scene key="profile" component={MyAnotherScene}  hideTabBar={true} hideNavBar={true} />
-                <Scene key="about" component={MyAnotherScene} hideNavBar={true} />
+                <Scene key="term" component={MyAnotherScene} hideTabBar hideNavBar />
+                <Scene key="profile" component={MyAnotherScene} hideTabBar hideNavBar />
+                <Scene key="about" component={MyAnotherScene} hideNavBar />
               </Scene>
+
             </Scene>
-            <Scene key="s2" component={MyAnotherScene} title="Scene2" />
+
+            <Scene key="s2" component={MyAnotherScene} hideTabBar hideNavBar />
+            <Scene key="s3" component={MyAnotherScene} hideTabBar hideNavBar />
+            <Scene key="s4" component={MyAnotherScene} hideTabBar hideNavBar />
+            <Scene key="s5" component={MyAnotherScene} hideTabBar hideNavBar />
           </Scene>
         </Scene>
       </Scene>
